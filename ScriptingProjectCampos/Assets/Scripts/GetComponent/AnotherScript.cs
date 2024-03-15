7c033dbf75efbf1d057f66e6450b429018b8913d
Gerardo Campos <751033@student.dusd.net>
2024-03-15 14:23:26 -0700

    Finished GetComponent

---
+++ ScriptingProjectCampos/Assets/Scripts/GetComponent/AnotherScript.cs
@@ -0,0 +1,10 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class AnotherScript : MonoBehaviour
+{
+    public int playerScore = 9001;
+}
